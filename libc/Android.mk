5ad43fb13cd14560dacfc56a78154b6d45e4e5dc
fusionjack <dogfight60-fusionjack@yahoo.de>
2013-12-13 21:32:04 +0100

    Add ARMv7 optimized string handling routines
    Change-Id: I41114d0107a60a1c2948778411f037bf09e4ac97

--- libc/Android.mk
+++ libc/Android.mk
@@ -368,7 +368,6 @@ libc_common_src_files += \
 	string/strncmp.c \
 	string/strncat.c \
 	string/strncpy.c \
-	bionic/strchr.cpp \
 	string/strrchr.c \
 	bionic/memrchr.c \
 	string/index.c \
@@ -432,7 +431,6 @@ libc_common_src_files += \
 	string/strcat.c \
 	string/strncat.c \
 	string/strncpy.c \
-	bionic/strchr.cpp \
 	string/strrchr.c \
 	bionic/memrchr.c \
 	string/index.c \
